bafbc9d89ab46591b5b360c8d367e69d660fa57c
anu <155603722+diffugient@users.noreply.github.com>
2024-08-09 00:16:20 +0530

    Create Dog.java

---
+++ java/Dog.java
@@ -0,0 +1,43 @@
+public class Dog 
+{
+    public static void main(String args[])
+    {
+        Dog1[] dogs = new Dog1[2];
+
+        dogs[0] = new Dog1("Race", "Husky");
+        dogs[1] = new Dog1("Tim", "Pomerian");
+
+        dogs[1].setter("Timothy", "Mongrel");
+
+        System.out.println("Name\tBreed");
+        int i;
+        for(i=0;i<2;i++)
+        {
+            dogs[i].display();
+        }
+
+    }
+}
+
+class Dog1
+{
+    String name;
+    String breed;
+
+    Dog1(String nm, String breed)
+    {
+        this.name = nm;
+        this.breed = breed;
+    }
+
+    void setter(String nm, String br)
+    {
+        name = nm;
+        breed = br;
+    }
+
+    void display()
+    {
+        System.out.println(name + "\t" + breed);
+    }
+}
