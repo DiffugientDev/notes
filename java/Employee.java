f059b507dfa0fc77c3ccfae95af6963bcdd85578
anu <155603722+diffugient@users.noreply.github.com>
2024-08-09 10:05:27 +0530

    Update and rename employeeProg.java to Employee.java

--- java/Employee.java
+++ java/Employee.java
@@ -1,13 +1,13 @@
-class employeeProg 
+class Employee
 {
     public static void main(String[] args)
     {
-        employee e1 = new employee("Anu","CEO");
-        employee e2 = new employee("Sarah","Employee");
-        employee e3 = new employee("Mira","Employee");
-        employee e4 = new employee("Zoe","Director");
-        employee e5 = new employee("Anne","Manager");
-        employee e6 = new employee("Aria","Employee");
+        Employee1 e1 = new Employee1("Anu","CEO");
+        Employee1 e2 = new Employee1("Sarah","Employee");
+        Employee1 e3 = new Employee1("Mira","Employee");
+        Employee1 e4 = new Employee1("Zoe","Director");
+        Employee1 e5 = new Employee1("Anne","Manager");
+        Employee1 e6 = new Employee1("Aria","Employee");
 
         System.out.println("Employee details: ");
         e1.display();
@@ -19,24 +19,16 @@ class employeeProg
 
 
         e3.updateSal("Manager");
-
-        System.out.println("\n\nAfter updating\nEmployee details: ");
-        e1.display();
-        e2.display();
-        e3.display();
-        e4.display();
-        e5.display();
-        e6.display();
     }
 
 }
 
-class employee
+class Employee1
 {
     String name, jobTitle;
     int salary;
 
-    employee(String n, String j)
+    Employee1(String n, String j)
     {
         this.name = n;
         this.jobTitle = j;
@@ -51,7 +43,7 @@ class employee
         else if(jobTitle.equals("Vice President"))
             salary = 8000000;
 
-        else if(jobTitle.equals("Drector"))
+        else if(jobTitle.equals("Director"))
             salary = 4000000;
 
         else if(jobTitle.equals("Manager"))
@@ -71,7 +63,7 @@ class employee
 
     public void display()
     {
-        System.out.println(name+" " + jobTitle + " " + salary);
+        System.out.println(name+" " + jobTitle + " " + salary + "\n");
     }
 
 }
