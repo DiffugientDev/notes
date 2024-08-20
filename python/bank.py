abbf2d6c1bcbd77186928536ff8a794ebb1bf724
Anushka Badhe <155603722+diffugient@users.noreply.github.com>
2024-08-20 13:35:40 +0530

    Create bank.py

---
+++ python/bank.py
@@ -0,0 +1,79 @@
+import os
+
+class Bank:
+    def __init__(self, aNum, balance):
+        self.acc_num = aNum
+        self.bal = balance
+
+    def showDetails(self):
+        print("Account number: ",self.acc_num)
+        print("Balance ",self.bal)
+
+    def debit(self):
+        num = int(input("Enter amt:  "))
+        if(self.bal - num >= 0):
+            self.bal -= num
+        else:
+            print("Balance is low")
+        self.showDetails()
+
+    def credit(self):
+        num = int(input("Enter amt:  "))
+        self.bal += num
+        self.showDetails()
+
+    def checkNum(self, accNo):
+        if(self.acc_num == accNo):
+            return True
+        else:
+            return False
+
+
+
+
+#main
+os.system("cls")
+
+customer = []
+
+for i in range(1,3):
+    print("\nCustomer ",i)
+    acc_num = input("Enter acc num: ") 
+    bal = int(input("Enter bank balance:  "))
+    cust1 = Bank(acc_num, bal)
+    customer.append(cust1)
+
+def validateAcc(acNum):
+    for i in range(len(customer)):
+        if customer[i].checkNum(acNum):
+            print(i)
+            return i
+    return -1
+
+while(True):
+    print("""\nOperations: 
+      1. Credit
+      2. Debit
+      3. Show Details
+      4. Exit""")
+    
+    choice = int(input("Enter a choice: "))
+    if(choice > 4):
+        print("Enter valid number")
+        continue
+
+    no = int(input("Enter account number:  "))
+    i = validateAcc(no)
+
+    if(i != -1):
+        match choice:
+            case 1:
+                customer[i].credit()
+            case 2:
+                customer[i].debit()
+            case 3:
+                customer[i].showDetails()
+            case 4:
+                exit(1)
+    else:
+        print("Enter valid account number")
