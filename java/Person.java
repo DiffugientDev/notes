027a4f887a5a7dd01f14fc02820c5a6aca2e547d
Anushka Badhe <155603722+diffugient@users.noreply.github.com>
2024-08-21 11:02:57 +0530

    Create Person.java

---
+++ java/Person.java
@@ -0,0 +1,33 @@
+/*
+  create a class person with 2 var name and age
+ write a prog to 
+    1. display details
+    2. find oldest person
+    3. sort persons by age
+ */
+
+import java.util.*;
+
+public class PersonSort
+{
+   int n = 2;
+   Person[] p = new Person[n];
+   for()
+}
+
+class Person
+{
+   String name;
+   int age;
+
+   Person(String n, int a)
+   {
+      this.name = n;
+      this.age = a;
+   }
+
+   public void display()
+   {
+      System.out.println("Name =  " + name + "Age = " + age);
+   }
+}
